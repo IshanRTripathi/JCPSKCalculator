7de83f39e60b5c29f7eb64299e3a88ff0db5bca8
ishanrtripathi <ishanrtripathi786@gmail.com>
2019-09-29 10:14:23 +0530

    Save Data Locally In Internal Storage Bug Status: Not Fixed Bug type: Array gets values in random order

--- app/src/main/java/com/ishanrtripathi/jcpskcalculator/MainActivity.java
+++ app/src/main/java/com/ishanrtripathi/jcpskcalculator/MainActivity.java
@@ -4,11 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     double rmList[]=new double[10];
     int tuList[]=new int[10];
     Button reset;
+
+    private static final String fileName="lastSavedData.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -307,7 +317,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        resetAll();
+//        resetAll();
+        try {
+            loadData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void resetAll() {
@@ -327,14 +342,14 @@ public class MainActivity extends AppCompatActivity {
         tv360.setText(String.valueOf(rmList[7]));
         tv125.setText(String.valueOf(rmList[8]));
         tv100.setText(String.valueOf(rmList[9]));
-        et150.setText("");//String.valueOf(tuList[0])
-        et175.setText("");//String.valueOf(tuList[1]));
-        et200.setText("");//String.valueOf(tuList[2]));
-        et225.setText("");//String.valueOf(tuList[3]));
-        et250.setText("");//String.valueOf(tuList[4]));
-        et275.setText("");//String.valueOf(tuList[5]));
-        et300.setText("");//String.valueOf(tuList[6]));
-        et360.setText("");//String.valueOf(tuList[7]));
+        et150.setText("");
+        et175.setText("");
+        et200.setText("");
+        et225.setText("");
+        et250.setText("");
+        et275.setText("");
+        et300.setText("");
+        et360.setText("");
         et125.setText("");
         et100.setText("");
     }
@@ -347,6 +362,11 @@ public class MainActivity extends AppCompatActivity {
         }
         totalUnits.setText(String.valueOf(s));
         updateTRM();
+        try {
+            saveData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void updateTRM()
@@ -358,5 +378,48 @@ public class MainActivity extends AppCompatActivity {
         }
         totalRM.setText(String.format(Locale.US,"%.5g%n",s));
         totalMT.setText(String.format(Locale.US,"%.4g%n",s/78.74));
+    }
+
+    private void saveData() throws IOException {
+        FileOutputStream fileOutputStream;
+        fileOutputStream=openFileOutput(fileName, MODE_PRIVATE);
+        byte[] contents=(String.valueOf(tuList[0])+"\n"+String.valueOf(tuList[1])+
+                "\n"+String.valueOf(tuList[2])+"\n"+String.valueOf(tuList[3])+
+                "\n"+String.valueOf(tuList[4])+"\n"+String.valueOf(tuList[5])+
+                "\n"+String.valueOf(tuList[6])+"\n"+String.valueOf(tuList[7])+
+                "\n"+String.valueOf(tuList[8])+"\n"+String.valueOf(tuList[9])).getBytes();
+        fileOutputStream.write(contents);
+//        fileOutputStream.write(et360.getText().toString().getBytes());
+
+//        Toast.makeText(this, "Saved : "+et360.getText().toString()+"\n"+getFilesDir()+"/"+fileName, Toast.LENGTH_LONG).show();
+    }
+
+    private void loadData() throws IOException{
+        FileInputStream fileInputStream;
+        fileInputStream=openFileInput(fileName);
+        StringBuilder data= new StringBuilder();
+        BufferedReader reader= new BufferedReader(new InputStreamReader(fileInputStream));
+
+        String text;
+
+        String []testArray;
+        while((text=reader.readLine())!=null)
+        {
+            data.append(text).append("\n");
+        }
+        testArray=data.toString().split("\n");
+
+//        Toast.makeText(this, ""+Arrays.toString(testArray), Toast.LENGTH_SHORT).show();
+        et360.setText(testArray[0].equals("0") ?"":testArray[0]);
+        et300.setText(testArray[1].equals("0") ?"":testArray[1]);
+        et275.setText(testArray[2].equals("0") ?"":testArray[2]);
+        et250.setText(testArray[3].equals("0") ?"":testArray[3]);
+        et225.setText(testArray[4].equals("0") ?"":testArray[4]);
+        et200.setText(testArray[5].equals("0") ?"":testArray[5]);
+        et175.setText(testArray[6].equals("0") ?"":testArray[6]);
+        et150.setText(testArray[7].equals("0") ?"":testArray[7]);
+        et125.setText(testArray[8].equals("0") ?"":testArray[8]);
+        et100.setText(testArray[9].equals("0") ?"":testArray[9]);
+        Toast.makeText(this, "Loaded "+ Arrays.toString(testArray), Toast.LENGTH_SHORT).show();
     }
 }
